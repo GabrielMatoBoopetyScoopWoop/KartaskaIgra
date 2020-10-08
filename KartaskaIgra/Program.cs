57580df1128e600b3b6e803e37dc7ea7efa1bf81
GabrielMato <58472077+GabrielMatoBoopetyScoopWoop@users.noreply.github.com>
2020-10-08 19:38:33 +0200

    initial commit

---
+++ KartaskaIgra/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KartaskaIgra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int karte, bodovi = 0;
+            while (bodovi < 31)
+            {
+                Console.Write("Upisite broj karte:");
+                karte = Convert.ToInt32(Console.ReadLine());
+                bodovi += karte;
+            }
+            if (bodovi == 31)
+            {
+                Console.WriteLine("Pobjedili ste!");
+            } else
+            {
+                Console.WriteLine("Izgubili ste!");
+            }
+        }
+    }
+}
